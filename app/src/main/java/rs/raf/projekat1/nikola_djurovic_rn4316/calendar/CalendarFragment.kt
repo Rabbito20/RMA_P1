f8bd67cc537658ff65fb0ad115451be9b5a61179
Nikola <djura97.nd@gmail.com>
2023-04-19 19:50:58 +0200

    Cleaned some imports in CalendarFragment.kt

--- app/src/main/java/rs/raf/projekat1/nikola_djurovic_rn4316/calendar/CalendarFragment.kt
+++ app/src/main/java/rs/raf/projekat1/nikola_djurovic_rn4316/calendar/CalendarFragment.kt
@@ -5,21 +5,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.RecyclerView
 import rs.raf.projekat1.nikola_djurovic_rn4316.R
-import rs.raf.projekat1.nikola_djurovic_rn4316.databinding.ActivityMainBinding
 import java.time.LocalDate
-import java.time.YearMonth
 import java.time.format.DateTimeFormatter
 
 class CalendarFragment : Fragment() {
 
     private lateinit var binding: CalendarFragment
 
-//    private var tvMonthYear: TextView? = binding.view?.findViewById(R.id.tvMonthDate)
+    //    private var tvMonthYear: TextView? = binding.view?.findViewById(R.id.tvMonthDate)
 //    var calendarRecyclerView: RecyclerView? = view?.findViewById(R.id.rvCalendar)!!
     lateinit var selectedDate: LocalDate
 
