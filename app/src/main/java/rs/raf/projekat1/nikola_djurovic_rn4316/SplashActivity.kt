9eb63d07f073f156ba2a87ab2d96d7b6c20a7464
Nikola <djura97.nd@gmail.com>
2023-04-19 19:45:45 +0200

    Implemented splash screen

---
+++ app/src/main/java/rs/raf/projekat1/nikola_djurovic_rn4316/SplashActivity.kt
@@ -0,0 +1,24 @@
+package rs.raf.projekat1.nikola_djurovic_rn4316
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        val handler = Handler(Looper.myLooper()!!)
+        handler.postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+            //  TODO: Remove delay
+        }, 3000)
+    }
+}
